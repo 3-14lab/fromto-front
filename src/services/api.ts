1822edd3863143b268c003aecc9ab2e472b99c98
AmauriLima <amauri.plimaj@gmail.com>
2022-03-07 23:52:32 -0300

    api link

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'https://api.appdepara.com.br/',
-  baseURL: 'http://localhost:3004/',
+  baseURL: 'https://api.appdepara.com.br/',
+  // baseURL: 'http://localhost:3004/',
 });
 
 export default api;
