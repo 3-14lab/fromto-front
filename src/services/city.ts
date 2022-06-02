e7bb22387f1f2fc80d2c171b84b8426fc4725a61
Matheus Oliveira <matheus.oliveira@ccc.ufcg.edu.br>
2022-06-02 19:07:27 -0300

    fix: remove console

--- src/services/city.ts
+++ src/services/city.ts
@@ -5,27 +5,18 @@ const createCity = async (name: string) => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const { user } = useAuth();
 
-  try {
-    const city = await api.post("city", {
-      name,
-      user_id: user.id,
-    });
+  const city = await api.post("city", {
+    name,
+    user_id: user.id,
+  });
 
-    return city;
-
-  } catch(err){
-    return console.log(err);
-  }
-}
+  return city;
+};
 
 const editCity = async (city_id: string, name: string) => {
-  try {
-    const response = await api.put(`city?city_id=${city_id}`, {name})
+    const response = await api.put(`city?city_id=${city_id}`, { name });
 
-    return response
-  } catch(error){
-    console.log(error)
-  }
-}
+    return response;
+};
 
-export { createCity, editCity }
+export { createCity, editCity };
